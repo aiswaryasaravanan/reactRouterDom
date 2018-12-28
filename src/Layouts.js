7cb01d68d4a93e6594a79b6406a818a4d4c81767
Aiswarya Saravanan <aiswaryasaravanan1998@gmail.com>
2018-12-28 11:33:40 +0530

    Router

---
+++ src/Layouts.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Layouts extends Component {
+  render() {
+    return (
+      <div>
+          <h1>Layouts</h1>
+      </div>
+    );
+  }
+}
+
+export default Layouts;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Layouts from './Layouts';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const navi=(
+    <Router>
+        <div>
+            <Route exact path='/' component={Home} />
+            <Route path='/About' component={About} />
+            <Route path='/Layouts' component={Layouts} />
+        </div>
+    </Router>
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(navi, document.getElementById('root'));
+
+// const routing = (
+//     <Router>
+//       <div>
+//         <Route exact path="/" component={App} />
+//         <Route path="/users" component={Users} />
+//         <Route path="/contact" component={Contact} />
+//       </div>
+//     </Router>
+//   )
+//   ReactDOM.render(routing, document.getElementById('root'))
+
